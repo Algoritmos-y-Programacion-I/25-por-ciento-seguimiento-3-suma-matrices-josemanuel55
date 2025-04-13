5c7a9dabf4a6d492a3cdaf67682fda54a7769d6f
Josemanuel55 <josecitoalvarez123@gmail.com>
2025-04-12 21:51:59 -0500

    si

--- src/model/MatrizNumerica.java
+++ src/model/MatrizNumerica.java
@@ -3,22 +3,31 @@ package model;
 public class MatrizNumerica {
 
     private int[][] matriz; // Declaro
-    private int cantFilas;
-    private int cantColumnas;
+    private int cantFilas; // Declaro
+    private int cantColumnas; // Declaro
 
-    public MatrizNumerica(int filas, int columnas) {
-        cantFilas = filas;
-        cantColumnas = columnas;
-        this.matriz = new int[filas][columnas]; // Inicializo
+    public MatrizNumerica(int filas, int columnas){
+        this.cantFilas = filas;
+        this.cantColumnas = columnas;
+        this.matriz = new int[filas][columnas]; // Inicializado
+
     }
 
-    public void insertarValor(int i, int j, int valor) {
+    public void instertarValor(int i, int j, int valor){
         matriz[i][j] = valor;
     }
 
     public int[][] getMatriz() {
         return matriz;
     }
-
-
+    
+    public MatrizNumerica sumar(MatrizNumerica otra) {
+        MatrizNumerica resultado = new MatrizNumerica(cantFilas, cantColumnas);
+        for (int i = 0; i < cantFilas; i++) {
+            for (int j = 0; j < cantColumnas; j++) {
+                resultado.instertarValor(i, j, this.matriz[i][j] + otra.getMatriz()[i][j]);
+            }
+        }
+        return resultado;
+    }
 }
